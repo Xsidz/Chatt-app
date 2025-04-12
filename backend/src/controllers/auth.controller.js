b0d172b7908e623098fa98c12c33890237b40936
Xsidz <siddhesh.kabraa@gmail.com>
2025-04-12 16:00:05 +0530

    Messagew routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -65,25 +66,46 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.log("Error in Login Controller", error.message)
-    res.status(500).json({message : "Internal Server Error"})
+    console.log("Error in Login Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const logout = (req, res) => {
- try {
-  res.cookie("jwt","",{maxAge:0})
-  res.status(200).json({message :"Logged Out Successfully"})
- } catch (error) {
-  console.log("error in logout controller", error.message)
-  res.status(500).json({message:"Internal Server Error"})
- }
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged Out Successfully" });
+  } catch (error) {
+    console.log("error in logout controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-export const updateProfile = async (req,res)=>{
+export const updateProfile = async (req, res) => {
   try {
-    
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      res.status(400).json({ message: "Profile Pic is required!" });
+    }
+
+    const uploadRespose = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic : uploadRespose.secure_url },{new : true})
+
+    res.status(200).json(updatedUser)
   } catch (error) {
-    
+
+ res.status(500).json({message :"Internal server Error"})
+  }
+};
+
+export const checkAuth = async (req,res)=>{
+  try {
+    res.status(200).json(req.user)
+  } catch (error) {
+    console.log("Error in the CheckAuth controller" , error.message)
+    res.status(500).json({message : "Internal Server Error"})
   }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authroutes from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authroutes);
+app.use("/api/message", messageRoute);
 
 app.listen(port, (req, res) => {
   console.log(`Bckedn running on port ${port}`);
