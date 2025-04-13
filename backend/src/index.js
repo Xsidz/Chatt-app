cfd162ccb35359ed04fcd6dd03d39aa82d35c5d6
Xsidz <siddhesh.kabraa@gmail.com>
2025-04-13 15:45:59 +0530

    signup, login functionlaity and ui done

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,13 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
-dotenv.config();
-const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
+dotenv.config();
+const port = process.env.PORT;
 app.use("/api/auth", authroutes);
 app.use("/api/message", messageRoute);
 
